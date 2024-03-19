406dd21654da5d8d83ab4069cfc27cc7e12b5ca4
Kasim Khan <kasimkkhan06@gmail.com>
2024-03-19 10:46:42 +0530

    final changes

--- AI/Expt4/8PuzzleUsingHillClimb.py
+++ AI/Expt4/8PuzzleUsingHillClimb.py
@@ -1,70 +1,83 @@
 import copy
+import numpy as np
+
 def findEmptySpot(currState):
     for i in range(len(currState)):
         for j in range(len(currState)):
             if currState[i][j] == 0 :
                 return i , j
             
+def findlocationinCurr(ele , currState):
+    for i in range(len(currState)):
+        for j in range(len(currState)):
+            if currState[i][j] == ele :
+                return i , j
+
+def findlocationinFinal(ele , finalState) :
+    for i in range(len(finalState)):
+        for j in range(len(finalState)):
+            if finalState[i][j] == ele :
+                return i , j
+
 def computeCost(currState , finalState):
     cost = 0
     for i in range(len(currState)):
         for j in range(len(finalState)):
-            if currState[i][j] != finalState[i][j] :
-                cost+=1
+            x1 , y1 = findlocationinCurr(currState[i][j] , currState)
+            x2 , y2 = findlocationinFinal(currState[i][j] , finalState)
+            cost += abs(x2-x1) + abs(y2-y1)
     return cost
 
-initial  = [[1,2,3],[5,6,0],[7,8,4]]
-final = [[1,2,3],[5,8,6],[0,7,4]]
+def _8PuzzleUsingHillClimb(initial , final):
+    #start the algorithm
+    #generate all the children nodes and check which gives the best value to explore further 
+    currState = copy.deepcopy(initial) #initialize the algorithm
 
-#start the algorithm
-#generate all the children nodes and check which gives the best value to explore further 
-currState = copy.deepcopy(initial) #initialize the algorithm
+    #To check up , right , down , left
+    drow = [-1 , 0 , +1 , 0] 
+    dcol = [0 , +1 , 0 , -1]
 
-#To check up , right , down , left
-drow = [-1 , 0 , +1 , 0] 
-dcol = [0 , +1 , 0 , -1]
+    h_x = computeCost(currState , final)
 
-h_x = 0 + computeCost(currState , final)
-level = 1
+    while True : 
 
-while True : 
-
-    #print the final selected Node
-    for i in range(len(currState)):
-        for j in range(len(currState)):
-            print(currState[i][j] , " " ,end="")
+        #print the final selected Node
+        for i in range(len(currState)):
+            for j in range(len(currState)):
+                print(currState[i][j] , " " ,end="")
+            print()
         print()
-    print()
 
-    #generate the children Nodes
-    #find the empty spot
-    if currState == final :
-        break
-    i , j = findEmptySpot(currState)
-    #move the surrounding four to current four states
-    childrenNode = copy.deepcopy(currState)
-    optimalState = copy.deepcopy(currState) #to store the best state among the chidren
-    found = False
-    for index in range(len(drow)) :
-        nrow = drow[index] + i
-        ncol = dcol[index] + j
-        #check if the new indices are valid
-        if nrow >= 0 and nrow < len(currState) and ncol >= 0 and ncol < len(currState) :
-            childrenNode[i][j] = currState[nrow][ncol]
-            childrenNode[nrow][ncol] = 0
-            #calculate the heuristic value 
-            newCost = level + computeCost(childrenNode , final)
-            if newCost <= h_x : #we found a better state
-                found = True
-                h_x = newCost
-                optimalState =  copy.deepcopy(childrenNode)
+        #generate the children Nodes
+        #find the empty spot
+        if currState == final :
+            break
+        i , j = findEmptySpot(currState)
+        #move the surrounding four to current four states
+        childrenNode = copy.deepcopy(currState)
+        optimalState = copy.deepcopy(currState) #to store the best state among the chidren
+        for index in range(len(drow)) :
+            nrow = drow[index] + i
+            ncol = dcol[index] + j
+            #check if the new indices are valid
+            if nrow >= 0 and nrow < len(currState) and ncol >= 0 and ncol < len(currState) :
+                childrenNode[i][j] = currState[nrow][ncol]
+                childrenNode[nrow][ncol] = 0
+                #calculate the heuristic value 
+                newCost = computeCost(childrenNode , final)
+                if newCost < h_x : #we found a better state
+                    h_x = newCost
+                    optimalState =  copy.deepcopy(childrenNode)
 
-            #reverse the changes
-            childrenNode[nrow][ncol] = childrenNode[i][j]
-            childrenNode[i][j] = 0
+                #reverse the changes
+                childrenNode[nrow][ncol] = childrenNode[i][j]
+                childrenNode[i][j] = 0
 
-    if not found : #we didnt get a better state than current
-        break
+        if currState == optimalState : #we didnt find any optimal state
+            break
 
-    currState = copy.deepcopy(optimalState)
-    level += 1
+        currState = copy.deepcopy(optimalState)
+
+initial  = [[1,2,0],[5,6,3],[7,8,4]]
+final = [[1,2,3],[5,8,6],[0,7,4]]
+_8PuzzleUsingHillClimb(initial , final)
